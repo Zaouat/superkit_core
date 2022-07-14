324b934d821ca0dc4a59b67caf4f9ee66fdf9b5b
Zaouat <zaouatabdellah@gmail.com>
2022-07-14 20:57:11 +0100

    add icon fix asstes path prob

--- lib/src/shared/constants.dart
+++ lib/src/shared/constants.dart
@@ -3,26 +3,25 @@
 library superkit.constants;
 
 /*                  Images                  */
-const String avatar = 'packages/superkit_library/assets/images/avatar.png';
+const String avatar = 'packages/superkit_core/assets/images/avatar.png';
 
-const String light_cover = 'packages/superkit_library/assets/images/light.png';
-const String dark_cover = 'packages/superkit_library/assets/images/dark.png';
-const String system_cover =
-    'packages/superkit_library/assets/images/system.png';
+const String light_cover = 'packages/superkit_core/assets/images/light.png';
+const String dark_cover = 'packages/superkit_core/assets/images/dark.png';
+const String system_cover = 'packages/superkit_core/assets/images/system.png';
 
 /*                        Marker Icons                   */
-const String pin = 'packages/superkit_library/assets/icons/gmap/pin.svg';
-const String plane = 'packages/superkit_library/assets/icons/gmap/plane.svg';
-const String truck = 'packages/superkit_library/assets/icons/gmap/truck.svg';
+const String pin = 'packages/superkit_core/assets/icons/gmap/pin.svg';
+const String plane = 'packages/superkit_core/assets/icons/gmap/plane.svg';
+const String truck = 'packages/superkit_core/assets/icons/gmap/truck.svg';
 const String locpinlock =
-    'packages/superkit_library/assets/icons/gmap/locpinlock.svg';
+    'packages/superkit_core/assets/icons/gmap/locpinlock.svg';
 const String thumbtack =
-    'packages/superkit_library/assets/icons/gmap/thumbtack.svg';
+    'packages/superkit_core/assets/icons/gmap/thumbtack.svg';
 
 /*                        MapMode Images                 */
 const String normal =
-    'packages/superkit_library/assets/images/map_mode/normal.png';
+    'packages/superkit_core/assets/images/map_mode/normal.png';
 const String satellite =
-    'packages/superkit_library/assets/images/map_mode/satellite.png';
+    'packages/superkit_core/assets/images/map_mode/satellite.png';
 const String terrain =
-    'packages/superkit_library/assets/images/map_mode/terrain.png';
+    'packages/superkit_core/assets/images/map_mode/terrain.png';
