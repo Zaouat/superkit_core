272faa99369b180fc434af89f994017bf4a09ee6
Zaouat <zaouatabdellah@gmail.com>
2023-11-01 23:17:37 +0100

    Add start page

---
+++ example/lib/pages/start.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:superkit_core/superkit_core.dart';
+
+class StartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SimpleSPageTemplate();
+  }
+}
