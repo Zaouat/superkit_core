381d7bf25e491a219d74518ab9a70c2e3e4a3f5a
Zaouat <zaouatabdellah@gmail.com>
2023-11-18 15:37:17 +0100

    remove new code

--- lib/src/widgets/s_settings/models/mobile/s_settings_mobile.dart
+++ lib/src/widgets/s_settings/models/mobile/s_settings_mobile.dart
@@ -1,10 +1,197 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+// import 'package:superkit_core/src/widgets/s_settings/models/mobile/appearance.dart';
+// import 'package:superkit_core/src/widgets/s_settings/models/mobile/drawer_switcher.dart';
+// import 'package:superkit_core/src/widgets/s_settings/models/mobile/language.dart';
+// import 'package:superkit_core/superkit_core.dart';
+
+// bool notification = true;
+
+// Future<void> superkitSettingsMobileModal({
+//   required BuildContext? context,
+//   required languagePr,
+//   required globalPr,
+//   drawerPr,
+//   required themePr,
+//   String? title,
+//   double? height,
+//   Color? darkColor,
+//   Color? lightColor,
+//   bool? hideonTap,
+//   bool? enableDrawer,
+//   bool? enbalethemes,
+//   final Function(bool)? onNotificationChange,
+//   bool? enableNotification = true,
+//   Widget? widgets,
+//   required List<Map<String, Object>>? supportedThemes,
+// }) {
+//   return showModalBottomSheet(
+//     isScrollControlled: true, // required for min/max child size
+//     context: context!,
+//     shape: const RoundedRectangleBorder(
+//       borderRadius: BorderRadius.only(
+//         topRight: Radius.circular(50),
+//         topLeft: Radius.circular(50),
+//       ),
+//     ),
+//     builder: (BuildContext ctx) {
+//       final String lang = Localizations.localeOf(context).languageCode;
+
+//       return ValueListenableBuilder<AdaptiveThemeMode>(
+//           valueListenable: AdaptiveTheme.of(context).modeChangeNotifier,
+//           builder: (_, Object? mode, Widget? child) {
+//             return Padding(
+//               padding: MediaQuery.of(context).viewInsets,
+//               child: Container(
+//                 height: height ?? MediaQuery.of(context).size.height / 1.5,
+//                 width: screenWidth(context),
+//                 decoration: BoxDecoration(
+//                   color: colorChanger(
+//                     context: context,
+//                     dark: darkColor ?? kcNeutralColor_100,
+//                     light: lightColor ?? Colors.white,
+//                   ),
+//                   borderRadius: const BorderRadius.only(
+//                     topRight: Radius.circular(50),
+//                     topLeft: Radius.circular(50),
+//                   ),
+//                   boxShadow: <BoxShadow>[
+//                     BoxShadow(
+//                       color: colorChanger(
+//                         context: context,
+//                         dark: Theme.of(context).primaryColor.withOpacity(0.08),
+//                         light: kcAccentColor_40.withOpacity(0.02),
+//                       ),
+//                       blurRadius: 12.0,
+//                       spreadRadius: 1.5,
+//                     ),
+//                   ],
+//                 ),
+//                 child: ListView(
+//                   physics: const BouncingScrollPhysics(),
+//                   children: <Widget>[
+//                     verticalSpaceSmall,
+//                     Container(
+//                       margin: EdgeInsets.only(
+//                         left: screenWidth(context) / 2.2,
+//                         right: screenWidth(context) / 2.2,
+//                       ),
+//                       height: 3,
+//                       decoration: BoxDecoration(
+//                         color: colorChanger(
+//                           context: context,
+//                           dark: Theme.of(context).primaryColor,
+//                           light: Theme.of(context).colorScheme.secondary,
+//                         ),
+//                         borderRadius:
+//                             const BorderRadius.all(Radius.circular(50)),
+//                       ),
+//                     ),
+//                     Material(
+//                       color: Colors.transparent,
+//                       child: Padding(
+//                         padding: const EdgeInsets.only(
+//                           left: 2,
+//                           right: 2,
+//                         ),
+//                         child: Column(
+//                           children: <Widget>[
+//                             verticalSpaceSmall,
+//                             SuperKitText.heading3Bold(
+//                               text: title ??
+//                                   AppLocalizations.of(context)!
+//                                       .translate('settings')!,
+//                               lang: lang,
+//                             ),
+//                             verticalSpaceRegular,
+//                             ListView(
+//                               physics: const BouncingScrollPhysics(),
+//                               primary: false,
+//                               shrinkWrap: true,
+//                               children: <Widget>[
+//                                 Appearance(
+//                                   sThemes: supportedThemes,
+//                                   hideonTap: hideonTap,
+//                                   themePr: themePr,
+//                                   enablethemes: enbalethemes,
+//                                 ),
+//                                 Visibility(
+//                                   visible: enableNotification!,
+//                                   child: Padding(
+//                                     padding: lang.contains('ar')
+//                                         ? const EdgeInsets.only(
+//                                             left: 22,
+//                                             right: 16,
+//                                           )
+//                                         : const EdgeInsets.only(
+//                                             left: 16,
+//                                             right: 22,
+//                                           ),
+//                                     child: Column(
+//                                       children: [
+//                                         verticalSpaceSmall,
+//                                         Row(
+//                                           children: [
+//                                             SuperKitText.bodySemiBold(
+//                                               text: AppLocalizations.of(
+//                                                       context)!
+//                                                   .translate(
+//                                                       'allow_notifications')!,
+//                                               lang: lang,
+//                                             ),
+//                                             const Spacer(),
+//                                             Switch.adaptive(
+//                                                 value: notification,
+//                                                 onChanged: (bool value) {
+//                                                   // mySetState(
+//                                                   //   () {
+//                                                   //     notification = value;
+//                                                   //     onNotificationChange!(
+//                                                   //         notification);
+//                                                   //   },
+//                                                   // );
+//                                                 })
+//                                           ],
+//                                         ),
+//                                       ],
+//                                     ),
+//                                   ),
+//                                 ),
+//                                 LanguageSection(
+//                                   languageProvider: languagePr,
+//                                   globalProvider: globalPr,
+//                                   hideonTap: hideonTap,
+//                                 ),
+//                                 enableDrawer!
+//                                     ? DrawerSwitcher(
+//                                         drawers: drawerPr.drawers,
+//                                         drawerProvider: drawerPr,
+//                                         defaultDrawer:
+//                                             drawerPr.getSelectedDrawer,
+//                                       )
+//                                     : Container(),
+//                                 verticalSpaceMedium,
+//                                 widgets ?? Container(),
+//                                 verticalSpaceMedium,
+//                               ],
+//                             ),
+//                           ],
+//                         ),
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             );
+//           });
+//     },
+//   );
+// }
+
+import 'package:flutter/Material.dart';
 import 'package:superkit_core/src/widgets/s_settings/models/mobile/appearance.dart';
 import 'package:superkit_core/src/widgets/s_settings/models/mobile/drawer_switcher.dart';
 import 'package:superkit_core/src/widgets/s_settings/models/mobile/language.dart';
 import 'package:superkit_core/superkit_core.dart';
-
-bool notification = true;
 
 Future<void> superkitSettingsMobileModal({
   required BuildContext? context,
@@ -14,14 +201,9 @@ Future<void> superkitSettingsMobileModal({
   required themePr,
   String? title,
   double? height,
-  Color? darkColor,
-  Color? lightColor,
+  Color? backgroundColor,
   bool? hideonTap,
   bool? enableDrawer,
-  bool? enbalethemes,
-  final Function(bool)? onNotificationChange,
-  bool? enableNotification = true,
-  Widget? widgets,
   required List<Map<String, Object>>? supportedThemes,
 }) {
   return showModalBottomSheet(
@@ -35,7 +217,6 @@ Future<void> superkitSettingsMobileModal({
     ),
     builder: (BuildContext ctx) {
       final String lang = Localizations.localeOf(context).languageCode;
-
       return ValueListenableBuilder<AdaptiveThemeMode>(
           valueListenable: AdaptiveTheme.of(context).modeChangeNotifier,
           builder: (_, Object? mode, Widget? child) {
@@ -45,11 +226,12 @@ Future<void> superkitSettingsMobileModal({
                 height: height ?? MediaQuery.of(context).size.height / 1.5,
                 width: screenWidth(context),
                 decoration: BoxDecoration(
-                  color: colorChanger(
-                    context: context,
-                    dark: darkColor ?? kcNeutralColor_100,
-                    light: lightColor ?? Colors.white,
-                  ),
+                  color: backgroundColor ??
+                      colorChanger(
+                        context: context,
+                        dark: kcNeutralColor_100,
+                        light: Colors.white,
+                      ),
                   borderRadius: const BorderRadius.only(
                     topRight: Radius.circular(50),
                     topLeft: Radius.circular(50),
@@ -112,49 +294,6 @@ Future<void> superkitSettingsMobileModal({
                                   sThemes: supportedThemes,
                                   hideonTap: hideonTap,
                                   themePr: themePr,
-                                  enablethemes: enbalethemes,
-                                ),
-                                Visibility(
-                                  visible: enableNotification!,
-                                  child: Padding(
-                                    padding: lang.contains('ar')
-                                        ? const EdgeInsets.only(
-                                            left: 22,
-                                            right: 16,
-                                          )
-                                        : const EdgeInsets.only(
-                                            left: 16,
-                                            right: 22,
-                                          ),
-                                    child: Column(
-                                      children: [
-                                        verticalSpaceSmall,
-                                        Row(
-                                          children: [
-                                            SuperKitText.bodySemiBold(
-                                              text: AppLocalizations.of(
-                                                      context)!
-                                                  .translate(
-                                                      'allow_notifications')!,
-                                              lang: lang,
-                                            ),
-                                            const Spacer(),
-                                            Switch.adaptive(
-                                                value: notification,
-                                                onChanged: (bool value) {
-                                                  // mySetState(
-                                                  //   () {
-                                                  //     notification = value;
-                                                  //     onNotificationChange!(
-                                                  //         notification);
-                                                  //   },
-                                                  // );
-                                                })
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                  ),
                                 ),
                                 LanguageSection(
                                   languageProvider: languagePr,
@@ -169,8 +308,6 @@ Future<void> superkitSettingsMobileModal({
                                             drawerPr.getSelectedDrawer,
                                       )
                                     : Container(),
-                                verticalSpaceMedium,
-                                widgets ?? Container(),
                                 verticalSpaceMedium,
                               ],
                             ),
